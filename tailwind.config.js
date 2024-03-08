f6f394da3b00ae46751b3fc96d85fad060970284
Fernando Rivera Gomez <fernando.rivera@dox.cl>
2024-03-08 09:12:16 -0500

    Uploading code to GitHub

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#FD4F00",
+        primary: "#8fc600",
+        phover: "#6aa84f",
+        fdox: {
+          gray: {
+            light: "#cccccc",
+            dark: "#6f6e6e"
+          }
+        }
+      },
+      fontFamily: {
+        sans: ["Inter var", "sans-serif"],
+        serif: ["Inter var", "serif"],
+        body: ["Inter var", "sans-serif"],
+        mono: [
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
+      }
+    },
+  },
+  plugins: [],
+}
+
